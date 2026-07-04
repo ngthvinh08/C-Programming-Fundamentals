f5bbdb629a0bfb30d036e12372b9ae48c4d42d95
ngthvinh08 <nguyenthanhvinh1613@gmail.com>
2026-07-04 18:11:46 +0700

    One more

---
+++ array/InsertAtIndex.c
@@ -0,0 +1,6 @@
+/******************************************************************
+Cho một mảng các số nguyên n phần tử arr được nhập từ bàn phím.
+Tiếp theo nhập vào vị trí k và giá trị x.
+Bạn hãy viết chương trình chèn x vào vị trí k và hiển thị mảng
+sau khi chèn.
+*******************************************************************/
