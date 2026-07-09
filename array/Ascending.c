5d3111fa63487432d9351c5a47a4eb8b4ae5f4a2
ngthvinh08 <nguyenthanhvinh1613@gmail.com>
2026-07-09 23:26:19 +0700

    Create swap function

--- array/Ascending.c
+++ array/Ascending.c
@@ -1,5 +1,24 @@
 /******************************************************************
 Cho một mảng các số nguyên n phần tử arr được nhập từ bàn phím.
 Bạn hãy viết chương trình kiểm tra xem mảng có được sắp xếp theo
-thứ tự tăng dần hay không.
+thứ tự tăng dần hay không. Nếu không thì sắp xếp lại theo thứ tự tăng
+dần rồi in ra màn hình kết quả.
 *******************************************************************/
+#include <stdio.h>
+
+/* Swap function */
+void swap(int *a, int *b){
+    int temp;
+    temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
+/* Function to check and re-arrange */
+
+int main(){
+    int c = 5;
+    int d = 10;
+
+    swap(&c, &d);
+}
