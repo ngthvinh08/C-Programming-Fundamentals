99d6bf137a4b55918464b1bdbbddf5021e1a8441
ngthvinh08 <nguyenthanhvinh1613@gmail.com>
2026-07-11 22:58:11 +0700

    Let main print not CheckOrder

--- array/Ascending.c
+++ array/Ascending.c
@@ -58,7 +58,6 @@ bool CheckOrder(int arr[], int n) {
 
     for (int i = 0; i < n - 1; i++) {
         if (arr[i] > arr[i + 1]) {
-            printf("Wrong order\n");
             return false;
         }
     }
@@ -102,6 +101,7 @@ if (status){
 
 /* Re-arrange if in wrong order */
 if (!status){
+    printf("Wrong order\n");
     ReArrange(arr, n);
     printf("Values after rearrange: ");
     for (int i = 0; i < n; i++){
