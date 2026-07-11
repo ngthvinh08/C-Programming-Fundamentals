7c390909a624639d9ac95d00f510d83b126544eb
ngthvinh08 <nguyenthanhvinh1613@gmail.com>
2026-07-11 23:02:35 +0700

    Loop multiple times - bubble sort

--- array/Ascending.c
+++ array/Ascending.c
@@ -43,11 +43,10 @@ void XORSwap(int *a, int *b) {
 
 /* Function to re-arrange */
 void ReArrange(int arr[], int n){
-    for (int i = 0; i < n; i++){
-        if (arr[i] > arr[i+1]){
-            Swap (&arr[i], &arr[i+1]);
-        }
-    }
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j+1])
+                Swap(&arr[j], &arr[j+1]);
 }
 
 /* Function to check if array is sorted in ascending order */
