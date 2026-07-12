4096d8aa8da1d8e90197b6ef18f25a5656c45061
ngthvinh08 <nguyenthanhvinh1613@gmail.com>
2026-07-12 23:41:50 +0700

    Done average

--- array/Avearge.c
+++ array/Avearge.c
@@ -3,4 +3,40 @@ Cho một mảng các số nguyên n phần tử arr được nhập từ bàn p
 Bạn hãy viết chương trình hiển thị ra giá trị trung bình cộng của
 các phần tử trong mảng.
 *******************************************************************/
+#include<stdio.h>
 
+float CalAverage(int arr[], int n){
+    float avg = 0;
+    for (int i = 0; i < n; i++){
+        avg += (float)arr[i]/n;
+    }
+
+    return avg;
+}
+
+int main(){
+
+    /* Input size */
+    int n;
+    printf ("Input size of the array: ");
+    scanf ("%d", &n);
+
+    /* Check n */
+    if (n <= 0){
+        printf ("There must be at least 1 element in the array");
+        return 1;
+    }
+
+    /* Input values of the array */
+    int arr[n];
+    printf ("Input values of the array: ");
+    for (int j = 0; j < n; j++){
+        scanf ("%d", &arr[j]);
+    }
+
+    /* Function call */
+    float avg = CalAverage(arr, n);
+    printf ("The average of values in array: %f\n", avg);
+
+    return 0;
+}
